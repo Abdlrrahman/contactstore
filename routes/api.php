6b7b16b7b3088675fad3aff5490fbfe24b12b260
Abdlrrahman <abdlrrahman.shibani@gmail.com>
2020-08-01 14:34:53 +0200

    created the needed backend routes

--- routes/api.php
+++ routes/api.php
@@ -2,17 +2,34 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Contact;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+Route::group(['middleware' => 'api'], function () {
+    // Fetch Contacts
+    Route::get('contacts', function () {
+        return Contact::latest()->orderBy('created_at','desc')->get();
+    });
+
+    // Fetch signle Contact
+    Route::get('contact/{id}', function ($id) {
+        return Contact::findOrFail($id);
+    });
+
+    // Add Contact
+    Route::post('contact/store', function (Requset $request) {
+        return Contact::create(['name'=>$request->input(['name']),'email'=>$request->input(['email']),'phone'=>$request->input(['phone'])]);
+    });
+
+    // Update Contact
+    Route::patch('contact/{id}', function (Requset $request, $id) {
+        Contact::findOrFail($id)->update(['name'=>$request->input(['name']),'email'=>$request->input(['email']),'phone'=>$request->input(['phone'])]);
+    });
+
+    // Delete Contact
+    Route::delete('contact/{id}', function ($id) {
+        return Contact::destroy($id);
+    });
+});
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
